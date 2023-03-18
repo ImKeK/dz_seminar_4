2bd301fdbad6f99ff429237e5a64b829c7bacc5e
*Egor <egorkiper21@gmail.com>
2023-03-18 12:10:10 +0300

    1 задача

---
+++ example001/Program.cs
@@ -0,0 +1,34 @@
+﻿int Metod(string message)
+{
+  System.Console.WriteLine(message);
+  string read = Console.ReadLine()!;
+  int result = Convert.ToInt32(read);
+  return result;
+}
+
+int Power(int powerBase, int exponent)
+{
+  int power = 1;
+  for(int i = 0; i < exponent; i++)
+  {
+    power *= powerBase;
+  }
+  return power;
+}
+
+bool ValidateExponent(int exponent)
+{
+  if(exponent < 0)
+  {
+    System.Console.WriteLine("Показатель не должен был быть меньше нуля.");
+    return false;
+  }
+  return true;
+}
+
+int powerBase = Metod("Введите основание: ");
+int exponent = Metod("Введите показатель степени: ");
+if(ValidateExponent(exponent))
+{
+  System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
